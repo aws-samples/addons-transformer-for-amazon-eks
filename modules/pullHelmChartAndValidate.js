9599d0fc54e415758309e870abc6b62154611e12
Elamaran Shanmugam <elamaran.shan@gmail.com>
2023-04-07 11:27:03 -0400

    Fixing formation of code

--- modules/pullHelmChartAndValidate.js
+++ modules/pullHelmChartAndValidate.js
@@ -17,47 +17,46 @@ async function pullHelmChartAndValidate(inputParameters) {
     registryIds: [ inputParameters.aws_accountid ]
     };
     ecr.getAuthorizationToken(loginParams, (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    const token = data.authorizationData[0].authorizationToken;
-    const decodedToken = Buffer.from(token, 'base64').toString().split(':');
-    const username = decodedToken[0];
-    const password = decodedToken[1];
-    const registryUrl = data.authorizationData[0].proxyEndpoint;
-    console.log("registryUrl : " +registryUrl)
-    const loginCmd = `helm registry login -u ${username} -p ${password} ${registryUrl}`;
-    // Log in to ECR
-    try {
-        const result = execSync(loginCmd);
-        console.log(result.toString());
-    } catch (error) {
-        console.error(error);
-        return;
-    }
-    // Pull the Helm chart from ECR
-    const pullCmd = `rm -rf ./unzipped-${addonName} && mkdir ./unzipped-${addonName} && helm pull ${helmUrl} --version ${addonVersion} --untar --untardir ./unzipped-${addonName}`;
-    try {
-        const result = execSync(pullCmd);
-        console.log(result.toString());
-        console.log('Helm Chart Pull is Successful!');
-    } catch (error) {
-        console.error(error);
-        return;
-    }
+        if (err) {
+            console.log(err);
+            return;
+        }
+        const token = data.authorizationData[0].authorizationToken;
+        const decodedToken = Buffer.from(token, 'base64').toString().split(':');
+        const username = decodedToken[0];
+        const password = decodedToken[1];
+        const registryUrl = data.authorizationData[0].proxyEndpoint;
+        console.log("registryUrl : " +registryUrl)
+        const loginCmd = `helm registry login -u ${username} -p ${password} ${registryUrl}`;
+        // Log in to ECR
+        try {
+            const result = execSync(loginCmd);
+            console.log(result.toString());
+            // Pull the Helm chart from ECR
+            const pullCmd = `rm -rf ./unzipped-${addonName} && mkdir ./unzipped-${addonName} && helm pull ${helmUrl} --version ${addonVersion} --untar --untardir ./unzipped-${addonName}`;
+            try {
+                const result = execSync(pullCmd);
+                console.log(result.toString());
+                console.log('Helm Chart Pull is Successful!');
+                // Search for occurrences of ".Capabilities" and "helm.sh/hook"
+                const findCapabilities = spawnSync('grep', ['-R', '-i', '-l', '-e', '".Capabilities"', `./unzipped-${addonName}`]);
+                const findHooks = spawnSync('grep', ['-R', '-i', '-l', '-e', '"helm.sh/hook"', `./unzipped-${addonName}`]);
+                // Check the counts and exit if either is greater than zero
+                if (findCapabilities.stdout > 0 || findHooks.stdout > 0) {
+                    console.log('Found .Capabilities or helm.sh/hook in Helm chart');
+                    process.exit(350);
+                    } else {
+                    console.log('No occurrences of .Capabilities or helm.sh/hook found in Helm chart');
+                }
+            } catch (error) {
+                console.error(error);
+                return;
+            }
+        } catch (error) {
+            console.error(error);
+            return;
+        }
     });
-
-    // Search for occurrences of ".Capabilities" and "helm.sh/hook"
-    const findCapabilities = spawnSync('grep', ['-R', '-i', '-l', '-e', '".Capabilities"', `./unzipped-${addonName}`]);
-    const findHooks = spawnSync('grep', ['-R', '-i', '-l', '-e', '"helm.sh/hook"', `./unzipped-${addonName}`]);
-    // Check the counts and exit if either is greater than zero
-    if (findCapabilities.stdout > 0 || findHooks.stdout > 0) {
-        console.log('Found .Capabilities or helm.sh/hook in Helm chart');
-        process.exit(350);
-        } else {
-        console.log('No occurrences of .Capabilities or helm.sh/hook found in Helm chart');
-    }
 }
 
 export default pullHelmChartAndValidate
